ec49882d0778bc30b3817aa049c105ee1659b38d
Marat Vildanov <mtmy@yandex.ru>
2025-02-04 19:42:14 +0500

    1

--- swapi.py
+++ swapi.py
@@ -30,21 +30,20 @@ class SWRequester(APIRequester):
        super().__init__()
 
     def get_sw_categories(self):
-        self.categories = self.session.get(self.base_url)
-        self.categories_dict = dict(self.categories.text)
-        print(f'категории {self.categories_dict.keys()}')
+        self.categories = list(self.session.get(self.base_url).json().keys())
+        #self.categories_dict = dict(self.categories.text)
+        print(f'категории {self.categories}')
         return self.categories
     
-    def get_sw_info(self, sw_type, url):
-        self.base_url = url
-        self.info = self.session.get(f'{self.base_url}{sw_type}')
+    def get_sw_info(self, sw_type):
+        self.info = self.session.get(f'{self.base_url}{sw_type}/')
         print(self.info.text)
         return self.info.text
     
 url_response = Response()
 url_response.get()
 sw_info = SWRequester()
-categories_list = sw_info.get_sw_categories()
+categories_list = SWRequester().get_sw_categories()
 SWRequester.get_sw_info(categories_list[0])
         
         
