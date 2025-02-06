21d201caf91fc7ff9e27947a6f7afae2b5f08dba
Marat Vildanov <mtmy@yandex.ru>
2025-02-06 21:02:07 +0500

    1

--- swapi.py
+++ swapi.py
@@ -7,11 +7,10 @@ class APIRequester:
 
     def __init__(self, base_url):
         self.base_url = base_url
-        self.session = requests.session()
 
     def get(self, url_add):
         try:
-            self.response = self.session.get(f'{self.base_url}/{url_add}')
+            self.response = requests.get(f'{self.base_url}/{url_add}')
             self.response.raise_for_status()
             return self.response
         except requests.ConnectionError:
@@ -22,8 +21,8 @@ class APIRequester:
 
 class SWRequester(APIRequester):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, base_url):
+        super().__init__(base_url)
 
     def get_sw_categories(self):
         self.categories = list(self.get('').json().keys())
@@ -42,7 +41,7 @@ class SWRequester(APIRequester):
 
 
 url_response = APIRequester('https://swapi.dev/api').get('')
-sw_info = SWRequester()
-categories_list = SWRequester().get_sw_categories()
-SWRequester().get_sw_info(categories_list[0])
-SWRequester().save_sw_info()
+sw_info = SWRequester('https://swapi.dev/api')
+categories_list = SWRequester('https://swapi.dev/api').get_sw_categories()
+SWRequester('https://swapi.dev/api').get_sw_info(categories_list[0])
+SWRequester('https://swapi.dev/api').save_sw_info()
